89c35d4f55302c044fb1905c82060712fc7d0d77
StephanieViannnaG <stephanieviannagnanasekaran@geakminds.com>
2023-04-05 17:23:00 +0530

    Added select, userInfo in header, Widget components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
 import MainApp from "./containers/MainApp";
 
 const App = () => {
-  return (
-    // <Routes>
-    //   <Route path="/" element={<MainApp />}></Route>
-    //   {/* <Route path="/hello" element={<HelloS val={"2"} />}></Route> */}
-    // </Routes>
-    <MainApp />
-  );
+  return <MainApp />;
 };
 
 export default App;
--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Avatar, Select } from "antd";
 
 const index = () => {
   return (
@@ -8,8 +8,27 @@ const index = () => {
         <Col span={2}>
           <div className="logo">Logo</div>
         </Col>
-        <Col span={20}>
+        <Col span={11}>
           <div className="appName">Product Name</div>
+        </Col>
+        <Col span={5}>
+          <Select defaultValue="lucy" className="headerSelect">
+            <Select.Option value="jack">Jack</Select.Option>
+            <Select.Option value="lucy">Lucy</Select.Option>
+          </Select>
+        </Col>
+        <Col span={4}>
+          <div className="userInfo">
+            <div className="userImage">
+              <Avatar size="large">U</Avatar>
+            </div>
+            <div className="userDesc">
+              <p className="userName">
+                <b>User Name</b>
+              </p>
+              <p className="userDesig">User Designation</p>
+            </div>
+          </div>
         </Col>
       </Row>
     </>
