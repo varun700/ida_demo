521e975d8e2ff5158351a71c928932b1c605974a
varun700 <varunmgeakminds@gmail.com>
2023-04-05 17:27:17 +0530

    redux toolkit setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import MainApp from "./containers/MainApp";
-
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "./AppRedux/slice/userslice";
 const App = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
   return (
     // <Routes>
     //   <Route path="/" element={<MainApp />}></Route>
     //   {/* <Route path="/hello" element={<HelloS val={"2"} />}></Route> */}
     // </Routes>
-    <MainApp />
+    <>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
+      <MainApp />
+    </>
   );
 };
 
